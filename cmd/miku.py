5f9df44a28859c838c7359d66b172363fc4c19b1
negaialan <87653224+negaialan@users.noreply.github.com>
2021-07-20 22:21:15 +0800

    Update miku.py

--- cmd/miku.py
+++ cmd/miku.py
@@ -7,8 +7,9 @@ import json
 with open('C:\\Users\\alann\\Documents\\GitHub\\dc\\setting.json','r',encoding='utf8') as jfile:
         jdata = json.load(jfile)
 
-class miku(Cog_Extension):
-
-    @commands.Cog.listener()
-    async def on_message(self, msg):
-        if msg.content == 'miku':
+class Pic(Cog_Extension):
+    @commands.command()
+    async def sampic(self, ctx):
+        random_pic = random.choice(jdata['miku'])
+        pic = discord.File(random_pic)
+        await ctx.send(file= pic)
